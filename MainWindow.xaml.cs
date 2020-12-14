1d865cd27eee98a4496770e6835c5961e6339206
Toke98 <73280669+Toke98@users.noreply.github.com>
2020-12-14 14:34:17 +0100

    Update

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,45 @@ namespace Uge_51_miniprojekt
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<Pizza> choice = new List<Pizza>(); 
+
         public MainWindow()
         {
             InitializeComponent();
+
+            choice .Add(new Pizza { pizzaName  = "Margarita", price = "alm: 50, fam: 100" , indgredienser = "tomat, ost, origano"});
+            choice .Add(new Pizza { pizzaName  = "Dirty Joe's", price = "alm: 75, fam: 150",indgredienser = "tomat, ost, sardiner, bacon, sild, nutella, origano" });
+            choice .Add(new Pizza { pizzaName  = "Skinke" , price ="alm: 60, fam: 120", indgredienser = "tomat, ost, skinke, origano" });
+
+            choice.ForEach(Console.WriteLine);
+
+            myComboBox.ItemsSource = choice;
+        }
+
+        //Denne void siger hvad der sker når man trykker på submit knappen
+        //private void submitButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    //Denne textbox fortæller dig hvad for en slags pizza du har bestilt
+        //    MessageBox.Show("Du vil gerne have en " + pizza.Text + " pizza");
+        //}
+
+        //Valg af pizza
+        public class Pizza
+        {
+
+            public string pizzaName { get; set; }
+
+            public string price { get; set; }
+
+            public string indgredienser { get; set; }
+
+            public string fullFizza
+            {
+                get
+                {
+                    return pizzaName + " pizza " + price + " " + indgredienser;
+                }
+            }
 
         }
     }
